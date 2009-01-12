b2ca64165ecf68b8d543abda0e6a39c663064fb9
Marc Fournier <marc.fournier@camptocamp.com>
2009-01-12 18:39:44 +0100

    new module puppet module, rsyncd.

---
+++ manifests/classes/rsyncd.pp
@@ -0,0 +1,42 @@
+class rsyncd {
+
+  realize(Package["rsync"])
+
+  file { "/etc/rsyncd.conf":
+    ensure => present,
+  }
+
+  augeas { "set rsyncd pidfile":
+    context => "/files/etc/rsyncd.conf/.anon/",
+    changes => "set 'pid file' /var/run/rsyncd.pid",
+    require => File["/etc/rsyncd.conf"],
+  }
+
+  case $operatingsystem {
+
+    Debian: {
+      augeas { "enable rsync service":
+        changes => "set /files/etc/default/rsync/RSYNC_ENABLE true",
+        onlyif => "get /files/etc/default/rsync/RSYNC_ENABLE != true",
+        notify => Service["rsync"],
+        require => Package["rsync"],
+      }
+      service { "rsync":
+        ensure => running,
+        enable => true,
+        require => Package["rsync"],
+      }
+    }
+
+    RedHat: {
+      augeas { "enable rsync service":
+        changes => "set /files/etc/xinetd.d/rsync/rsync/disable no",
+        onlyif => "get /files/etc/xinetd.d/rsync/rsync/disable != no",
+        notify => Service["xinetd"],
+        require => Package["xinetd"],
+      }
+    }
+
+  }
+
+}
---
+++ manifests/definitions/export.pp
@@ -0,0 +1,73 @@
+define rsyncd::export ($ensure=present, $chroot=true, $readonly=true, $path=undef, $uid=undef, $gid=undef, $users=undef, $secrets=undef, $allow=undef, $deny=undef) {
+
+  $ctx = "/files/etc/rsyncd.conf"
+
+  case $ensure {
+    present: {
+
+      if $path {
+        augeas { "setup rsyncd export $name":
+          changes => [
+            "set '$ctx/$name/#comment' 'created by rsyncd::export($name)'",
+            "set '$ctx/$name/path' '$path'",
+            "set '$ctx/$name/use chroot' $chroot",
+            "set '$ctx/$name/read only' $readonly",
+          ],
+          require => Augeas["set rsyncd pidfile"],
+        }
+
+        if $uid {
+          augeas { "set rsyncd uid for $name":
+            changes => "set '$ctx/$name/uid' $uid",
+            require => Augeas["setup rsyncd export $name"],
+          }
+        }
+
+        if $gid {
+          augeas { "set rsyncd gid for $name":
+            changes => "set '$ctx/$name/gid' $gid",
+            require => Augeas["setup rsyncd export $name"],
+          }
+        }
+
+        if $users {
+          augeas { "set rsyncd auth users for $name":
+            changes => "set '$ctx/$name/auth users' '$users'",
+            require => Augeas["setup rsyncd export $name"],
+          }
+        }
+
+        if $secrets {
+          augeas { "set rsyncd secrets file for $name":
+            changes => "set '$ctx/$name/secrets file' '$secrets'",
+            require => Augeas["setup rsyncd export $name"],
+          }
+        }
+
+        if $allow {
+          augeas { "set rsyncd hosts allow for $name":
+            changes => "set '$ctx/$name/hosts allow' '$allow'",
+            require => Augeas["setup rsyncd export $name"],
+          }
+        }
+
+        if $deny {
+          augeas { "set rsyncd hosts deny for $name":
+            changes => "set '$ctx/$name/hosts deny' '$deny'",
+            require => Augeas["setup rsyncd export $name"],
+          }
+        }
+
+      }
+      else {
+        fail("missing mandatory \$path parameter for rsyncd::export($path).")
+      }
+
+    }
+    absent: {
+      augeas { "remove $name":
+        changes => "remove '$ctx/$name'",
+      }
+    }
+  }
+}
